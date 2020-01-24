3b53a173b8a49413a5832fdec72dd073fc89c2c3
ajay143444 <ajay_singh10@infosys.com>
2020-01-24 14:58:55 +0530

    Add files via upload

---
+++ constants/constants.py
@@ -0,0 +1,8 @@
+#Copyright 2018 Infosys Ltd.
+#Use of this source code is governed by Apache 2.0 license that can be found in the LICENSE file or at
+#http://www.apache.org/licenses/LICENSE-2.0  . 
+####DATABASE QUERY STATUS CODES####
+CON000 = 'CON000' # Successfull database connection
+CON001 = 'CON001' # Failed to connect to database
+EXE000 = 'EXE000' # Successful query execution
+EXE001 = 'EXE001' # Query Execution failure
